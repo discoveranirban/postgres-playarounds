38c490f690c27bb28aac5fb8d127dda8a4a3e42b
discoveranirban <discoveranirban@gmail.com>
2023-09-12 00:46:58 +0530

    added api routes and db calls for the same

--- src/repos/user-repo.js
+++ src/repos/user-repo.js
@@ -1,4 +1,5 @@
 const pool = require('../pool');
+const { use } = require('../routes/users');
 
 class UserRepo {
   static async find() {
@@ -8,16 +9,35 @@ class UserRepo {
   }
 
   static async findById(id) {
-    const { rows } = await pool.query(`SELECT * FROM users WHERE id = ${id};`);
+    const { rows } = await pool.query(`SELECT * FROM users WHERE id = $1;`, [id]);
 
     return rows[0];
   }
 
-  static async insert() {}
+  static async insert(username, bio) {
+    const { rows } = await pool.query(`INSERT INTO users (username, bio) VALUES ($1, $2) RETURNING *`, [username, bio]);
 
-  static async update() {}
+    return rows[0];
+  }
 
-  static async delete() {}
+  static async update(id, username, bio) {
+    const {
+      rows,
+    } = await pool.query(
+      'UPDATE users SET username = $1, bio = $2 WHERE id = $3 RETURNING *;',
+      [username, bio, id]
+    );
+
+    return rows[0];
+  }
+
+  static async delete(id) {
+    const {
+      rows,
+    } = await pool.query('DELETE FROM users WHERE id = $1 RETURNING *;', [id]);
+
+    return rows[0];
+  }
 }
 
 module.exports = UserRepo;
